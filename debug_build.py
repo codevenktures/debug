e3d02e53834935136b0ac5cb6a8ec6ea360654a7
Venkata Edarapalli <vnitin-x@interactive1.condorcomputing.net>
2023-12-06 06:18:45 -0600

    added gaurds to inputs

--- debug_build.py
+++ debug_build.py
@@ -10,9 +10,18 @@ if(len(sys.argv) < 2):
 	print(start_time)
 	sys.exit()
 
-output_file = sys.argv[1]
-monitor_path = sys.argv[2]
-start_time = sys.argv[3]
+if(len(sys.argv) >= 2):
+	monitor_path = sys.argv[1]
+else:
+	monitor_path = os.getcwd()
+if(len(sys.argv) >= 3):
+	output_file = sys.argv[2]
+else:
+	output_file = "debug_of"
+if(len(sys.argv) >= 4):
+	start_time = sys.argv[3]
+else:
+	start_time = 100
 
 arr = [monitor_path]
 my_dict = {}
@@ -22,7 +31,10 @@ while(len(arr) > 0):
 	darr = os.listdir(d)
 	for df in darr:
 		p = f"{d}/{df}"
-		if(not(os.path.islink(p))):
+		if(os.path.islink(p)):
+			link_stat = os.lstat(p)
+			my_dict[p] = st_ctime
+		if(os.path.exits()):
 			my_dict[p] = os.path.getctime(p)
 			if(os.path.isdir(p)):
 				arr.append(p)
@@ -31,6 +43,12 @@ filtered_keys = [key for key, value in my_dict.items() if value > float(start_ti
 
 output_dir = sorted_keys = sorted(filtered_keys, key=lambda x: my_dict[x])
 
-f = open(output_file, "w")
+f = open(output_file+"_size", "w")
 for od in output_dir:
 	f.write(od+"  --> "+str(os.path.getsize(od))+"\n")
+f.close()
+
+f = open(output_file, "w")
+for od in output_dir:
+	f.write(od+"\n")
+f.close()
